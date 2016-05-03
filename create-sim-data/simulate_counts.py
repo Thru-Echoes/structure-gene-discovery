dc2e68e5e3389c223fb9c9c6efc3127080514d7c
halexand <halexand@mit.edu>
2016-05-03 16:09:16 -0400

    updated beta-prop values for user

--- create-sim-data/simulate_counts.py
+++ create-sim-data/simulate_counts.py
@@ -60,11 +60,11 @@ def read_input(input):
 	df = pd.DataFrame.from_csv(input)
 	return df
 
-def property_beta_distribution(n_props):
+def property_beta_distribution(n_props, beta_prop_1, beta_prop_2):
 	# Samples from beta probability distribution
 	# Output: matrix of probabilities (size n_props)
 
-	property_probs = beta.rvs(1, 5, size = n_props)
+	property_probs = beta.rvs(beta_prop_1, beta_prop_2, size = n_props)
 	probabilities = np.array(property_probs)
 	probs_shape = probabilities.reshape(1, n_props)
 	return probs_shape
@@ -123,8 +123,10 @@ if __name__ == "__main__":
 	parser.add_argument("-mf", "--multi_fact", help = "Set multiplicative factor for negative binomial distribution", type = int)
 	parser.add_argument("-af", "--add_fact", help = "Set additive factor for negative binomial distribution")
 	parser.add_argument("-nf", "--num_flips", help = "Set number of flips for negative binomial distribution")
-	parser.add_argument("-b1", "--beta_gene_1", help = "Set number of flips for negative binomial distribution")
-	parser.add_argument("-b2", "--beta_gene_2", help = "Set number of flips for negative binomial distribution")
+	parser.add_argument("-b1", "--beta_gene_1", help = "Set beta distribution a value for genes")
+	parser.add_argument("-b2", "--beta_gene_2", help = "Set beta distribution b value for genes")
+	parser.add_argument("-p1", "--beta_prop_1", help = "Set beta distribution a value for props")
+	parser.add_argument("-p2", "--beta_prop_2", help = "Set beta distribution a value for props")
 
 	args = parser.parse_args()
 
@@ -148,6 +150,15 @@ if __name__ == "__main__":
 	else:
 		beta_gene_2 = int(args.beta_gene_2)
 
+	if args.beta_prop_1 is None:
+		beta_prop_1 = 1
+	else:
+		beta_prop_1 = int(args.beta_prop_1)
+
+	if args.beta_prop_2 is None:
+		beta_prop_2 = 5
+	else:
+		beta_prop_2 = int(args.beta_prop_2)
 
 
 	if args.n_species is None:
@@ -179,7 +190,7 @@ if __name__ == "__main__":
 		#species_properties = read_input(input_file)
 		species_prop, gp_sign, gp_break = simulate_species_properties(n_props, n_species, n_genes)
 		n_props = len(species_prop.axes[1])
-		properties = property_beta_distribution(n_props)
+		properties = property_beta_distribution(n_props, beta_prop_1, beta_prop_2)
 		genes = gene_beta_distribution(n_genes, beta_gene_1, beta_gene_2)
 		probability_distribution = np.dot(genes, properties)
 		coin_toss_data = coin_toss(probability_distribution)
